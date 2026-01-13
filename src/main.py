19cfe996e2ab5962c191d0406c8b42303cf2f41f
CupChunGae <c_nim@hotmail.com>
2026-01-13 14:56:10 +0000

    Fixed the textnode class

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
-print("hello world")
-
 from textnode import *
+
 def main():
-    example = TextNode("This is some anchor", "link", "https://www.boot.dev")
-    return example.__repr__
+    example = TextNode("This is some anchor", TextType.LINK, "https://www.boot.dev")
+    print(example.__repr__())
 
 main()
